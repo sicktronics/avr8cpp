9c5651f8140260a9ecb3845cbff0d4ffdb6a2234
Parker Caywood Mayer <thisisseriousbizniz@gmail.com>
2024-11-12 19:26:51 -0800

    Fixed a type

--- src/CPU/interrupt.h
+++ src/CPU/interrupt.h
@@ -5,4 +5,4 @@
 /*
 This function emulates how the CPU would handle an interrupt by saving the current program counter (PC) to the stack, updating the stack pointer, and jumping to a specified address (interrupt vector).
 */
-void avrInterrupt(CPU *cpu, u16 addrress);
+void avrInterrupt(CPU *cpu, u16 address);
